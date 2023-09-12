45599805d912008498d4081452b343718e814877
Lindvar Lagran <llag@equinor.com>
2023-09-12 16:47:22 +0200

    logg

--- backend/main3.py
+++ backend/main3.py
@@ -37,7 +37,7 @@ listbox = ListBox(left_box, items=[drink["Name"] for drink in service.get_drink_
                   command=display_drink_details, width=400, height=400)
 
 # A button to print the details of the selected drink
-print_button = PushButton(left_box, text="Print Drink Details", command=print_drink_details(listbox.value), width=400)
+print_button = PushButton(left_box, text="Print Drink Details", command=lambda : service.make_drink(listbox.value), width=400)
 
 #print_button = PushButton(left_box, text="Print Drink Details", command=service.make_drink(listbox.value), width=400)
 
