a2e3eea226c5e91c7979bf973d6683a4f31582d8
Lindvar Lagran <llag@equinor.com>
2023-09-12 16:30:02 +0200

    call make drinks

--- backend/drink_service.py
+++ backend/drink_service.py
@@ -132,6 +132,7 @@ class DrinkService:
             ml = value[0]
             self.pump_service.run_pump(pump_number, ml)
 
+
 if __name__ == '__main__':
     a = DrinkService()
     a.make_drink("Gin Fizz")
