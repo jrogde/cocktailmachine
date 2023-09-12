d6f21027f1b47ce562522069f5643951ee11670e
Lindvar Lagran <llag@equinor.com>
2023-09-12 14:50:23 +0200

    drink_service.py

--- backend/drink_service.py
+++ backend/drink_service.py
@@ -5,42 +5,117 @@ logger = logging.getLogger(__name__)
 
 
 class DrinkService:
-    drink = {
-        "recipes": {
-            "Gin and Tonic (GT)": {
-                "ingredients": {
-                    "Gin": "50 ml",
-                    "Tonic Water": "150 ml"
-                }
-            },
-            "Sex on the Beach": {
-                "ingredients": {
-                    "Vodka": "50 ml",
-                    "Peach Schnapps": "30 ml",
-                    "Cranberry Juice": "90 ml",
-                    "Orange Juice": "90 ml",
-                    "Grenadine Syrup": "15 ml"
-                }
-            },
-            "Mojito": {
-                "ingredients": {
-                    "White Rum": "60 ml",
-                    "Fresh Lime Juice": "30 ml",
-                    "Simple Syrup": "15 ml",
-                    "Soda Water": "120 ml"
-                }
+    drink_ingredients = {
+        1: "Vodka",
+        2: "Rum",
+        3: "Tequila",
+        4: "Gin",
+        5: "Whiskey",
+        6: "Triple Sec",
+        7: "Lime Juice",
+        8: "Lemon Juice",
+        9: "Orange Juice",
+        10: "Cranberry Juice",
+        11: "Grenadine",
+        12: "Simple Syrup",
+        13: "Cola",
+        14: "Tonic Water",
+        15: "Club Soda",
+        16: "Bitters"
+    }
+
+    drink_types = [
+        {
+            "Name": "Tequila Sunrise",
+            "Ingredients": {
+                3: (60, "ml"),  # Tequila
+                9: (90, "ml"),  # Orange Juice
+                11: (15, "ml")  # Grenadine
+            }
+        },
+        {
+            "Name": "Gin and Tonic",
+            "Ingredients": {
+                4: (60, "ml"),  # Gin
+                14: (90, "ml")  # Tonic Water
+            }
+        },
+        {
+            "Name": "Vodka Cranberry",
+            "Ingredients": {
+                1: (60, "ml"),  # Vodka
+                10: (90, "ml")  # Cranberry Juice
+            }
+        },
+        {
+            "Name": "Whiskey Sour",
+            "Ingredients": {
+                5: (60, "ml"),  # Whiskey
+                7: (30, "ml"),  # Lemon Juice
+                12: (15, "ml")  # Simple Syrup
+            }
+        },
+        {
+            "Name": "Rum Punch",
+            "Ingredients": {
+                2: (60, "ml"),  # Rum
+                9: (30, "ml"),  # Orange Juice
+                11: (15, "ml")  # Grenadine
+            }
+        },
+        {
+            "Name": "Vodka Soda",
+            "Ingredients": {
+                1: (60, "ml"),  # Vodka
+                15: (90, "ml")  # Club Soda
+            }
+        },
+        {
+            "Name": "Mojito",
+            "Ingredients": {
+                2: (60, "ml"),  # Rum
+                7: (30, "ml"),  # Lime Juice
+                12: (30, "ml"),  # Simple Syrup
+                15: (90, "ml")  # Club Soda
+            }
+        },
+        {
+            "Name": "Moscow Mule",
+            "Ingredients": {
+                1: (60, "ml"),  # Vodka
+                7: (30, "ml"),  # Lime Juice
+                16: (90, "ml")  # Ginger Beer
+            }
+        },
+        {
+            "Name": "Gin Fizz",
+            "Ingredients": {
+                4: (60, "ml"),  # Gin
+                7: (30, "ml"),  # Lemon Juice
+                12: (15, "ml"),  # Simple Syrup
+                15: (90, "ml")  # Club Soda
+            }
+        },
+        {
+            "Name": "Rum and Cola",
+            "Ingredients": {
+                2: (60, "ml"),  # Rum
+                13: (90, "ml")  # Cola
             }
         }
-    }
+    ]
+
     #def __init__(self):
 
-    def get_drinks(self):
-        return self.drink
+    def get_drink_ingredients(self):
+        return self.drink_ingredients
+
+    def get_drink_types(self):
+        return self.drink_types
 
 
 if __name__ == '__main__':
     a = DrinkService()
-    drink = a.get_drinks()
-    print(drink)
+    print(a.get_drink_types())
 
 
--- backend/pump_service.py
+++ backend/pump_service.py
@@ -1,4 +1,5 @@
 import logging
+import smbus2 as smbus
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
@@ -12,10 +13,25 @@ class PumpService:
         self.a = test
         logger.debug(f'asda')
 
-    def run_it(self, ):
-        logger.info(f'a: {self.a}')
+    def number_to_bytes(self, number):
+        if number < 0 or number > 65535:
+            raise ValueError("Number must be between 0 and 65535 (16-bit range)")
+        # Extract the two bytes using bitwise operations
+        byte1 = (number >> 8) & 0xFF
+        byte2 = number & 0xFF
+        return [byte1, byte2]
+
+    def run_pump(self, pump_number):
+        send = [pump_number, *self.number_to_bytes(1000)]
+        logger.debug(f'pump_number {send}')
+        try:
+            bus = smbus.SMBus(1)
+            bus.write_block_data(0x04, send)
+        except Exception as e:
+            print(f"Error communicating with Arduino: {e}")
 
 
 if __name__ == '__main__':
     a = PumpService('a')
-    a.run_it()
+
+    a.run_pump(1)
