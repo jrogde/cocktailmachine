c6cab4d8363880522945c6001b1d7a0a0882c565
Lindvar Lagran <llag@equinor.com>
2023-09-12 14:31:50 +0200

    backen

--- backend/drink_service.py
+++ backend/drink_service.py
@@ -6,11 +6,41 @@ logger = logging.getLogger(__name__)
 
 class DrinkService:
     drink = {
-
+        "recipes": {
+            "Gin and Tonic (GT)": {
+                "ingredients": {
+                    "Gin": "50 ml",
+                    "Tonic Water": "150 ml"
+                }
+            },
+            "Sex on the Beach": {
+                "ingredients": {
+                    "Vodka": "50 ml",
+                    "Peach Schnapps": "30 ml",
+                    "Cranberry Juice": "90 ml",
+                    "Orange Juice": "90 ml",
+                    "Grenadine Syrup": "15 ml"
+                }
+            },
+            "Mojito": {
+                "ingredients": {
+                    "White Rum": "60 ml",
+                    "Fresh Lime Juice": "30 ml",
+                    "Simple Syrup": "15 ml",
+                    "Soda Water": "120 ml"
+                }
+            }
+        }
     }
-
     #def __init__(self):
 
-    def get_drinks(self, ):
-        logger.info(f'a: {self.a}')
+    def get_drinks(self):
+        return self.drink
+
+
+if __name__ == '__main__':
+    a = DrinkService()
+    drink = a.get_drinks()
+    print(drink)
+
 
